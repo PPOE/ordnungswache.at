cdfb4230ca81f9acd2e6d3cb72711b2e888a3fd2
PeterTheOne <petertheone@gmail.com>
2017-01-15 20:00:30 +0100

    update with code from murkraftwerk.at

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 
   $(function () {
 
+    if (navigator.userAgent.match(/iPhone|iPad|Android/i)) {
+      $('.share-btn.whatsapp,.share-btn.facebook-messenger').css('display', 'inline-block');
+    }
+
     var $questions = $('.content .question');
 
 
@@ -13,11 +17,10 @@
       $(this).css('transform', 'translateX(-50%) rotate(' + ((Math.random() * 5) - 4) + 'deg)');
     });
 
-
-
-
-    var $randomQ = $questions.eq(Math.round(Math.random() * $questions.length) - 1);
-    $randomQ.show();
+    $questions.removeClass('show');
+    var randomNumber = Math.round(Math.random() * $questions.length) - 1;
+    var $randomQ = $questions.eq(randomNumber);
+    $randomQ.addClass('show');
 
     $('button.positive', $questions).on('click', function (event) {
       if ($('article.positive', $questions).hasClass('show')) {
@@ -52,6 +55,21 @@
       if (event.keyCode === 27) { // esc
         $('article', $questions).removeClass('show');
       }
+    });
+
+    $('.nextQuestion').on('click', function(event) {
+      event.preventDefault();
+      $('.question article').removeClass('show');
+      $questions.removeClass('show');
+
+      var oldRandomNumber = randomNumber;
+      do {
+        randomNumber = Math.round(Math.random() * $questions.length) - 1;
+        //console.log('round: ' + oldRandomNumber + ' - ' + randomNumber);
+      } while (oldRandomNumber === randomNumber);
+
+      $randomQ = $questions.eq(randomNumber);
+      $randomQ.addClass('show');
     });
 
 
